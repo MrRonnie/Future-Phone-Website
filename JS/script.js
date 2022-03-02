125ac5c9106f8d3fa85e0f245d781003382d2c23
MrRonnie <ronymaza55@gmail.com>
2022-03-02 16:01:44 +0600

    5th commit details update

--- JS/script.js
+++ JS/script.js
@@ -16,7 +16,7 @@ const searchPhone = () => {
     .then ((data) => displaySearchResult(data.data));
 
 }
-
+// Display Search Result 
 const displaySearchResult = (data) => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
@@ -34,7 +34,7 @@ const displaySearchResult = (data) => {
           </div>
           <div class="d-grid mx-auto w-75 my-3 ">
 
-            <button type="button" class="btn btn-outline-primary fs-6  fw-bold ">See Feature</button>
+            <button onclick="featuresData('${phone.slug}')" type="button" class="btn btn-outline-primary fs-6  fw-bold ">See Features</button>
 
            </div>
         </div>
@@ -43,4 +43,25 @@ const displaySearchResult = (data) => {
 
     }
 
+}
+
+const featuresData = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id} `;
+    fetch(url)
+    .then ((responsea) => responsea.json())
+    .then ((data) => displayPhoneDetails (data.data));
+}
+
+// Display details or features result 
+const displayPhoneDetails = (infos) =>{
+    document.getElementById("details-container").innerHTML = `
+    <div class="p-2 shadow p-3 bg-body rounded card h-100">
+              <img src="${infos.image}" class="card-img-top mt-3 w-50 m-auto" alt="">
+          <div class="card-body">
+              <h4 class="phone-name">${infos.name}</h4>
+              <p class="brand-name">${infos.brand}</p>
+          </div>
+         
+    </div>
+    `;
 }
